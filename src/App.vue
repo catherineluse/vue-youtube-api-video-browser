35fecd1f1941b47c6614b8a1a7afa87234b435a0
cluse <catherine.luse@gmail.com>
2021-02-15 20:28:42 -0700

    Add README and screenshots

--- src/App.vue
+++ src/App.vue
@@ -5,50 +5,55 @@
 
     <div class="row">
       <VideoDetail :video="selectedVideo"></VideoDetail>
-      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+      <VideoList
+        @videoSelect="onVideoSelect"
+        :videos="videos"
+      ></VideoList>
     </div>
-    </div>
+  </div>
 
 </template>
 
 <script type="text/javascript">
-  import axios from 'axios';
-  import SearchBar from './components/SearchBar';
-  import VideoList from './components/VideoList';
-  import VideoDetail from './components/VideoDetail'
-  let API_KEY = require('./config.js');
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+let API_KEY_object = require("./config.js");
+let API_KEY = API_KEY_object["default"];
 
-  export default {
-    name: "App",
-    components: {
-      SearchBar,
-      VideoList,
-      VideoDetail
-    },
-    data(){
-      return {
-        videos: [],
-        selectedVideo: null
-      }
-    },
-    methods: {
-      onTermChange(searchTerm){
-        axios.get('https://www.googleapis.com/youtube/v3/search', {
+export default {
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail,
+  },
+  data() {
+    return {
+      videos: [],
+      selectedVideo: null,
+    };
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
           params: {
             key: API_KEY,
-            type: 'video',
-            part: 'snippet',
-            q: searchTerm
-          }
-        }).then(response => this.videos = response.data.items)
-      },
-      onVideoSelect(video){
-        this.selectedVideo = video;
-      }
-    }
-  }
+            type: "video",
+            part: "snippet",
+            q: searchTerm,
+          },
+        })
+        .then((response) => (this.videos = response.data.items));
+    },
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
+  },
+};
 </script>
 
 <style media="screen">
-
 </style>
